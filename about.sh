0aa186278578c54ed3b86c99228d2c4f05d2c55d
NSTFREENET-ID <68092423+nstfreenet@users.noreply.github.com>
2020-07-12 09:07:55 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "# Script Auto Install SSH & OpenVPN             #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 32 bit & 64 bit                  #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified NSTFREENET                          #"
+echo -e "#-----------------------------------------------#"
+echo -e "Thanks To"
+echo -e "---------"
+echo -e "Allah"
+echo -e "My Family"
+echo -e "Google"
+echo -e "Admin Portal & White Vps"
+echo -e "Horasss"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
